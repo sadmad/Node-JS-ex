ee84af0b7a24d67baee997db0d13a2b23798ffd1
sadmad <sarmad.re@gmail.com>
2020-05-21 00:31:55 +0200

    some works to understand json inject

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ////////////////File
 
@@ -26,10 +27,28 @@ const http = require('http');
 // console.log('Will reading File!')
 
 ////////////////Server
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
+const productData = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server!');
+    const pathName =req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product'){
+        res.end('This is the PRODUCT');
+    } else if (pathName === '/api'){
+
+        res.writeHead(200, { 'Content-type': 'application/json'})
+        res.end(data);
+
+        
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8080, '127.0.0.1', () =>{
